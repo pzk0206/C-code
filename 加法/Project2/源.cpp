9b80a1c6d15435880c034c8c2920fe97225a7c9a
庞镇坤 <904903879@qq.com>
2022-02-16 14:35:36 +0800

    加法

---
+++ 加法/Project2/源.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int a = 11;
+	int b ;
+	int c ;
+	scanf(" %d", &b);
+	c = a + b;
+	printf("%d\n", c);
+	printf("a=%d\n", a);
+		return 0;
+	  
+}
