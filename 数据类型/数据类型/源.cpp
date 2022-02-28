0a3248ba0f1e1a40872ca738ea0ac65d234665a9
庞镇坤 <904903879@qq.com>
2022-02-28 08:14:46 +0800

    数据类型

---
+++ 数据类型/数据类型/源.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+    printf("%d\n", sizeof(char));
+    printf("%d\n",sizeof(short));
+    printf("%d\n",  sizeof(int));
+    printf("%d\n",  sizeof(long));
+    printf("%d\n",  sizeof(long long));
+    printf("%d\n",  sizeof(float));
+    printf("%d\n",  sizeof(double));
+    printf("%d\n",  sizeof(long double));
+    return 0;
+}
