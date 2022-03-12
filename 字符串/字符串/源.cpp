3ede7adc6a48ef0d13a2340d9d205ccfb0770374
庞镇坤 <904903879@qq.com>
2022-03-12 17:44:36 +0800

    字符串

---
+++ 字符串/字符串/源.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//下面代码，打印结果是什么？为什么？（突出'\0'的重要性）
+int main()
+{
+    char arr1[] = "bit";
+    char arr2[] = { 'b', 'i', 't' };
+    char arr3[] = { 'b', 'i', 't','\0'};
+    printf("%s\n", arr1);
+    printf("%s\n", arr2);
+    printf("%s\n", arr3);   
+    return 0;
+}
